1b0716903d276f7be66ef7ede73b057f60a1140d
Aneirine <anyashi2002@gmail.com>
2021-03-28 18:18:04 +0300

    Add configuration beeans

--- src/main/java/com/aneirine/demo/config/PersistenceConfig.java
+++ src/main/java/com/aneirine/demo/config/PersistenceConfig.java
@@ -1,11 +1,33 @@
 package com.aneirine.demo.config;
 
+import com.mysql.cj.jdbc.MysqlDataSource;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+
+import javax.sql.DataSource;
+
 
 @Configuration
 @MapperScan("com.aneirine.demo.mapper")
 public class PersistenceConfig {
 
+    @Bean
+    public DataSource dataSource() {
+        return new EmbeddedDatabaseBuilder()
+                .addScript("schema.sql")
+                .addScript("data.sql")
+                .build();
+    }
 
+    @Bean
+    public SqlSessionFactory sqlSessionFactory() throws Exception {
+        SqlSessionFactoryBean factoryBean = new SqlSessionFactoryBean();
+        factoryBean.setDataSource(dataSource());
+        return factoryBean.getObject();
+    }
 }
