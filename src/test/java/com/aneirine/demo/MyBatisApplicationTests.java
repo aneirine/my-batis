4f2bee4869923921ad61e84e26ecae0a826205aa
Aneirine <anyashi2002@gmail.com>
2021-03-28 17:13:27 +0300

    Initial commit

---
+++ src/test/java/com/aneirine/demo/MyBatisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aneirine.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBatisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
